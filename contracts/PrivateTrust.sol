2e9aa7a712754e76b6420a64b49630afc490e697
kmart13 <kmart@kenitram.com>
2017-10-16 08:51:09 -0600

    Development of private trust contract

--- contracts/PrivateTrust.sol
+++ contracts/PrivateTrust.sol
@@ -1,13 +1,60 @@
 pragma solidity ^0.4.17;
 
 contract PrivateTrust {
+  struct Beneficiary {
+    string name;
+    uint maturityAge;
+  }
+
   address public trustor;
   address public trustee;
+
+  Beneficiary[] private beneficiaries;
+
+  event LogDeposit(uint amount);
 
   modifier onlyTrustor() { require(msg.sender == trustor); _; }
   modifier onlyTrustee() { require(msg.sender == trustee); _; }
 
   function PrivateTrust() public { trustor = msg.sender; }
+
+  /// Trustor can assign a trustee at address `_addr`.
+  function assignTrustee(address _addr) public onlyTrustor {
+    trustee = _addr;
+  }
+
+  /** Designate a new beneficiary with their full legal name `_name`,
+    * and how old `_age` they are required to be before withdrawing from the trust.
+    * If the beneficiary has already been designated, this will update the maturityAge to `_age`.
+    */
+  function designateBeneficiary(string _name, uint _age) public onlyTrustor {
+    var (exists, index) = findBeneficiary(_name);
+
+    if (exists) {
+      beneficiaries[index].maturityAge = _age;
+    } else {
+      beneficiaries.push(Beneficiary(_name, _age));
+    }
+  }
+
+  /// Trustor can deposit funds to the trust and this event will be logged
+  function deposit() public payable onlyTrustor {
+    LogDeposit(msg.value);
+  }
+
+  function getBalance() public view returns (uint) {
+    return this.balance;
+  }
+
+  function findBeneficiary(string _name) private view returns (bool, uint) {
+    for (uint i = 0; i < beneficiaries.length; i++) {
+      if (keccak256(beneficiaries[i].name) == keccak256(_name)) {
+        return (true, i);
+      }
+    }
+
+    return (false, 0);
+  }
 
   function() public { }
 
--- test/privatetrust.js
+++ test/privatetrust.js
@@ -3,7 +3,39 @@ var PrivateTrust = artifacts.require("./PrivateTrust.sol");
 const web3 = global.web3;
 
 contract('PrivateTrust', function(accounts) {
+  var event;
+  var trustor = accounts[0];
+  var trustee = accounts[1];
+
   beforeEach(async function() {
-    trust = await PrivateTrust.new({from: accounts[0]});
+    accounts[0].balance = 1000000000000000000000000000;
+    trust = await PrivateTrust.new({from: trustor});
+    await trust.assignTrustee(trustee, {from: trustor});
   });
-}
+
+  afterEach(async function() {
+    event.stopWatching();
+  });
+
+  it("should allow the trustor to deposit to the contract", async function() {
+    var amount = web3.toWei(1, "ether");
+
+    event = trust.LogDeposit(function(error, result) {});
+
+    await trust.deposit({from: trustor, value: amount});
+    assert.equal(await trust.getBalance(), amount, "The private trust should contain the deposited amount");
+
+    console.log(event.get()[0].event + ": " + event.get()[0].args.amount.valueOf() + " wei.");
+    console.log("Trust Balance: " + await trust.getBalance() + " wei.");
+  });
+
+  it("should allow the trustor to assign a new trustee", async function() {
+    await trust.assignTrustee(accounts[2], {from: trustor});
+    assert.equal(await trust.trustee(), accounts[2], "The trustor should be able to assign a new trustee address");
+    await trust.assignTrustee(trustee, {from: trustor});
+    assert.equal(await trust.trustee(), trustee, "The trustor should be able to assign a new trustee address");
+  });
+
+  it("should allow the trustor to designate a new beneficiary", async function() {
+  });
+});
